a816a2835563bc59c4dd0a7368e1ae560023c961
chridahl <christoffer.dahl@gmail.com>
2018-05-29 23:57:59 +0200

    Changes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# ml-setup
+## ml-check
 
-To verify installed programs and python modules:
+A small script to verify that a pre-selected stack for developing and running various ml-tech is installed.
+
+To run the verification script:
 
 ```bash
 wget https://github.com/chridahl/ml-setup/blob/master/ml-check.sh
@@ -8,3 +10,6 @@ chmod +x ml-check.sh
 ./ml-check.sh
 ```
 
+## ml-setup
+
+Todo: Create script to download, install and setup required software.
--- ml-check.sh
+++ ml-check.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-programs=(python python3 pip3 pytorch jupyter)
-python_packages=(scipy math numpy pandas matplotlib keras lightgbm xgboost sklearn)
+programs=(python pip python3 pip3 jupyter)
+python_packages=(scipy math numpy pandas sklearn matplotlib tensorflow keras lightgbm xgboost torch torchvision)
+has_python2=false
+has_python3=false
+python_program="python"
+has_pip2=false
+has_pip3=false
 
-echo "Checking for installed programs..."
+printf "\n🕵️   Checking for installed programs...\n\n"
 for program in "${programs[@]}"
 do
-    echo -n "Checking ${program}..."
+    #echo -n "Checking ${program}..."
     if command -v ${program} >/dev/null 2>&1; then
-        echo "${program} found. 👌"
+        echo "✔️   ${program}"
+        if [ "$program" == "python" ]; then
+            has_python2=true
+        fi
+        if [ "$program" == "python3" ]; then
+            has_python3=true
+        fi  
+        if [ "$program" == "pip" ]; then
+            has_pip2=true
+        fi
+        if [ "$program" == "python3" ]; then
+            has_pip3=true
+        fi        
     else 
-        echo "${program} not found. 🛑"
+        echo "⚠️   ${program} not found."
     fi
 done
 
-echo "Checking for installed python modules..."
+if [ "$has_python3" == true ]; then
+    python_program="python3"
+fi    
+
+printf "\n🕵️   Checking for installed python modules...\n\n"
 for python_package in "${python_packages[@]}"
 do
-    echo -n "Checking ${python_package}..."
-    if python -c "import ${python_package}" >/dev/null 2>&1; then
-        echo "${python_package} found. 👌"
+    #echo -n "Checking ${python_package}..."
+    if "$python_program" -c "import ${python_package}" >/dev/null 2>&1; then
+        echo "✔️   ${python_package}"
     else 
-        echo "${python_package} not found. 🛑"
+        echo "⚠️   ${python_package} not found."
     fi
 done
